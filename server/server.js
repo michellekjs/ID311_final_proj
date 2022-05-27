1aa3189e54c36db0f221ab483070b01ea9cfaf12
jj139811 <jj139811@kaist.ac.kr>
2022-05-27 20:22:28 +0900

    Implemented simple ping pong with test client

--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
 const express = require('express');
-
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
-const port = 3001;
-
-app.set('port', port);
+const PORT = 3001;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('<h1>Hello World!</h1>');
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('a user disconnected');
+    });
+    socket.on('ping', (arg) => {
+        console.log('Received ping: ' + String(arg));
+
+        io.emit('pong', 'hi!');
+    })
 });
 
-app.listen(port, () => console.log('Listening on', port));
-
-module.exports = app;
+server.listen(PORT, () => {
+    console.log('listening on port: ' + String(PORT));
+});
