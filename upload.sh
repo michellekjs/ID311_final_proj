745e669c1ed5f289084d1d7e5415d8d8c4f1607b
jj139811 <jj139811@kaist.ac.kr>
2022-05-31 11:28:09 +0900

    moved client folder into server folder

--- upload.sh
+++ upload.sh
@@ -1,7 +1,4 @@
 rm server.tar.gz
-rm client.tar.gz
 tar -czvf server.tar.gz server
-tar -czvf client.tar.gz client
 scp -i id311-project-aws-ec2.pem server.tar.gz ec2-user@52.79.235.185:
-scp -i id311-project-aws-ec2.pem client.tar.gz ec2-user@52.79.235.185:
 echo "upload completed"
